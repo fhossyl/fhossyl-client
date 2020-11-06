93cbe5ce4c72258f1cafb704189e3ae423c72ed1
nekkan <nxkkdev@gmail.com>
2020-11-06 09:20:12 -0300

    Add Kotlin/Multiplatform build configuration.

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    kotlin("multiplatform") version "1.4.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+
+    linuxX64()
+
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+    
+    js {
+        nodejs {
+            binaries.executable()
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit5"))
+                implementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+                runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+            }
+        }
+        val linuxX64Main by getting
+        val linuxX64Test by getting
+        val nativeMain by getting
+        val nativeTest by getting
+        val jsMain by getting
+        val jsTest by getting
+    }
+
+}
