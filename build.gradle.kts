b7d4da91153d859b9e55286acff881473711b130
nekkan <nxkkdev@gmail.com>
2020-11-06 09:34:44 -0300

    Add packets subproject.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,61 +2,67 @@ plugins {
     kotlin("multiplatform") version "1.4.10"
 }
 
-repositories {
-    mavenCentral()
-}
+allprojects {
 
-kotlin {
+    plugins.apply("org.jetbrains.kotlin.multiplatform")
 
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
+    repositories {
+        mavenCentral()
     }
 
-    linuxX64()
+    kotlin {
 
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-    
-    js {
-        nodejs {
-            binaries.executable()
-        }
-    }
-
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
+        jvm {
+            compilations.all {
+                kotlinOptions.jvmTarget = "1.8"
+            }
+            testRuns["test"].executionTask.configure {
+                useJUnitPlatform()
             }
         }
-        val jvmMain by getting
-        val jvmTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit5"))
-                implementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-                runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+
+        linuxX64()
+
+        val hostOs = System.getProperty("os.name")
+        val isMingwX64 = hostOs.startsWith("Windows")
+
+        val nativeTarget = when {
+            hostOs == "Mac OS X" -> macosX64("native")
+            hostOs == "Linux" -> linuxX64("native")
+            isMingwX64 -> mingwX64("native")
+            else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+        }
+
+        js {
+            nodejs {
+                binaries.executable()
             }
         }
-        val linuxX64Main by getting
-        val linuxX64Test by getting
-        val nativeMain by getting
-        val nativeTest by getting
-        val jsMain by getting
-        val jsTest by getting
+
+        sourceSets {
+            val commonMain by getting
+            val commonTest by getting {
+                dependencies {
+                    implementation(kotlin("test-common"))
+                    implementation(kotlin("test-annotations-common"))
+                }
+            }
+            val jvmMain by getting
+            val jvmTest by getting {
+                dependencies {
+                    implementation(kotlin("test-junit5"))
+                    implementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+                    runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+                }
+            }
+            val linuxX64Main by getting
+            val linuxX64Test by getting
+            val nativeMain by getting
+            val nativeTest by getting
+            val jsMain by getting
+            val jsTest by getting
+        }
+
     }
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "fhossyl-client"
+include("packets")
