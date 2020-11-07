9b40ce3f48794fb4013ef6b6b45e641114f660c5
nekkan <nxkkdev@gmail.com>
2020-11-07 15:36:36 -0300

    Update PacketIO.kt.

--- packets/common/src/commonMain/kotlin/org/fhossyl/packets/common/PacketIO.kt
+++ packets/common/src/commonMain/kotlin/org/fhossyl/packets/common/PacketIO.kt
@@ -4,18 +4,35 @@ import io.ktor.utils.io.core.*
 import kotlin.experimental.and
 import kotlin.js.JsName
 
-object PacketIO {
+abstract class PacketIO {
 
-    @JsName("value0x7FL")
-    private const val `0x7FL`: Byte = 0x7FL.toByte()
+    companion object {
+        @JsName("value0x7FL")
+        private const val `0x7FL`: Byte = 0x7FL.toByte()
 
-    @JsName("zero")
-    private const val `0`: Byte = 0
+        @JsName("zero")
+        private const val `0`: Byte = 0
 
-    @JsName("value0x80")
-    private const val `0x80`: Byte = 0x80.toByte()
+        @JsName("value0x80")
+        private const val `0x80`: Byte = 0x80.toByte()
+    }
 
-    fun decode(buffer: Input): Long {
+    open fun decodeString(buffer: Input, maxStringLength: Int = 0): String {
+        val length = decode(buffer).toInt()
+            .takeIf { it > 0 && maxStringLength <= 0 || it < maxStringLength * 4 }
+            ?: error("Unsupported buffer length for decodeString.")
+
+        return buffer.readBytes(length).decodeToString().takeIf { it.length < maxStringLength }
+            ?: error("The string length should be lower than $maxStringLength.")
+    }
+
+    open fun decodeVector3F(buffer: Input): Vector3F {
+        return with(buffer) {
+            Vector3F(readFloatLittleEndian(), readFloatLittleEndian(), readFloatLittleEndian())
+        }
+    }
+
+    open fun decode(buffer: Input): Long {
         var result: Long = 0
         var shift = 0
         while(shift < 64) {
@@ -29,7 +46,7 @@ object PacketIO {
         throw ArithmeticException("VarInt or VarLong was too large")
     }
 
-    fun encode(buffer: Output, value: Long) {
+    open fun encode(buffer: Output, value: Long) {
         var value = value
         while(true) {
             value = if(value and 0x7FL.inv() == 0L) {
--- packets/common/src/commonMain/kotlin/org/fhossyl/packets/common/Vector3F.kt
+++ packets/common/src/commonMain/kotlin/org/fhossyl/packets/common/Vector3F.kt
@@ -1,6 +1,6 @@
 package org.fhossyl.packets.common
 
-data class Vector3F(val x: Double, val y: Double, val z: Double)
+data class Vector3F(val x: Float, val y: Float, val z: Float)
 
 inline operator fun Vector3F.plus(vector: Vector3F) = Vector3F(x + vector.x, y + vector.y, z + vector.z)
 
