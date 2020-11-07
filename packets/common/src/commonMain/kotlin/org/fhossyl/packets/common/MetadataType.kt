21bb9107913d5ec48aa27ced8c25250c751c446f
nekkan <nxkkdev@gmail.com>
2020-11-07 06:49:45 -0300

    Create MetadataType.kt.

---
+++ packets/common/src/commonMain/kotlin/org/fhossyl/packets/common/MetadataType.kt
@@ -0,0 +1,16 @@
+package org.fhossyl.packets.common
+
+enum class MetadataType {
+
+    Flags,
+    Byte,
+    Short,
+    Int,
+    Float,
+    String,
+    Long,
+    NBT,
+    Vector3I,
+    Vector3F;
+
+}
