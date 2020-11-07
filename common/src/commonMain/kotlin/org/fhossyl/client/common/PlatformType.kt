be7fab2b26424b7595a694dba6e1d96abd59d4bc
nekkan <nxkkdev@gmail.com>
2020-11-07 06:35:35 -0300

    Create PlatformType.kt.

---
+++ common/src/commonMain/kotlin/org/fhossyl/client/common/PlatformType.kt
@@ -0,0 +1,13 @@
+package org.fhossyl.client.common
+
+/**
+ * Enum class that stores all platforms that Fhossyl could be run.
+ */
+enum class PlatformType {
+
+    Android,
+    Standalone,
+    // BungeeCord,
+    Bukkit;
+
+}
