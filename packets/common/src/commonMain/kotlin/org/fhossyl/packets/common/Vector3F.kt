6a7f2d8a24f26bcc9460574f4e87d033e29dae7c
nekkan <nxkkdev@gmail.com>
2020-11-08 05:27:03 -0300

    Create typealias Location for Vector3F.

--- packets/common/src/commonMain/kotlin/org/fhossyl/packets/common/Vector3F.kt
+++ packets/common/src/commonMain/kotlin/org/fhossyl/packets/common/Vector3F.kt
@@ -2,6 +2,8 @@ package org.fhossyl.packets.common
 
 data class Vector3F(val x: Float, val y: Float, val z: Float)
 
+typealias Location = Vector3F
+
 inline operator fun Vector3F.plus(vector: Vector3F) = Vector3F(x + vector.x, y + vector.y, z + vector.z)
 
 inline operator fun Vector3F.minus(vector: Vector3F) = Vector3F(x - vector.x, y - vector.y, z - vector.z)
